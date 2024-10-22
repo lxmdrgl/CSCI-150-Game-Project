e73cb2ed5e495db2bf3ad4262c1dd1c72d2c22b1
Chester Lee <114546460+FinalHope007@users.noreply.github.com>
2024-10-21 22:35:17 -0700

    Implemented Basic Pause Menu
    Implemented basic pause menu with continue, options and quit button. Quit button not yet link with the title screen UI. Created the options menu with back button and two placeholder buttons (Gamepad Controls & Keyboard Controls)

--- Assets/Scripts/Player/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -6,13 +6,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    private PlayerInput playerInput;
+    public static PlayerInput playerInput;
     
     public Vector2 RawMovementInput { get; private set; }
 
     public int NormInputX { get; private set; }
     public int NormInputY { get; private set; }
     public bool JumpInput { get; private set; }
+    public bool MenuOpenInput { get; private set; }
+    public bool UIMenuCloseInput { get; private set; }
 
 
     private void Start()
@@ -39,7 +41,7 @@ public class PlayerInputHandler : MonoBehaviour
         {
             JumpInput = true;
         }
-
+        
         if (context.canceled)
         {
             JumpInput = false;
@@ -47,4 +49,36 @@ public class PlayerInputHandler : MonoBehaviour
     }
 
     public void UseJumpInput() => JumpInput = false;
+
+    public void OnMenuOpenInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            MenuOpenInput = true;
+            
+        }
+        
+        if (context.canceled)
+        {
+            MenuOpenInput = false;
+        }
+    }
+
+    public void UseMenuOpenInput() => MenuOpenInput = false;
+
+     public void OnUIMenuCloseInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            UIMenuCloseInput = true;
+            
+        }
+        
+        if (context.canceled)
+        {
+            UIMenuCloseInput = false;
+        }
+    }
+
+    public void UseUIMenuCloseInput() => UIMenuCloseInput = false;
 }
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    
+    [SerializeField] private GameObject mainMenuCanvasGO;
+    [SerializeField] private GameObject optionsMenuCanvasGO;
+    
+    [SerializeField] private GameObject mainMenuFirst;
+    [SerializeField] private GameObject optionsMenuFirst;
+
+    private bool isPaused;
+
+    public PlayerInputHandler InputHandler { get; private set; }
+    private PlayerInput playerInput;
+
+    void Start()
+    {
+        InputHandler = player.GetComponent<PlayerInputHandler>();
+        playerInput = player.GetComponent<PlayerInput>();
+        
+        mainMenuCanvasGO.SetActive(false);
+        optionsMenuCanvasGO.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (InputHandler.MenuOpenInput)
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+        }
+        else if (InputHandler.UIMenuCloseInput)
+        {
+            if (isPaused)
+            {
+                Unpause();
+            }
+        }
+    }
+
+    #region Pause/Unpause Functions
+    public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+
+        playerInput.SwitchCurrentActionMap("UI");
+
+        OpenMainMenu();
+    }
+
+    public void Unpause()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+
+        playerInput.SwitchCurrentActionMap("Player");
+
+        CloseAllMenus();
+    }
+    #endregion
+
+    #region Canvas Activations/Deactivations
+    private void OpenMainMenu()
+    {
+        mainMenuCanvasGO.SetActive(true);
+        optionsMenuCanvasGO.SetActive(false);
+
+        InputHandler.UseMenuOpenInput();
+
+        EventSystem.current.SetSelectedGameObject(mainMenuFirst);
+    }
+
+    private void OpenOptionsMenuHandle()
+    {
+        mainMenuCanvasGO.SetActive(false);
+        optionsMenuCanvasGO.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(optionsMenuFirst);
+    }
+
+    private void CloseAllMenus()
+    {
+        mainMenuCanvasGO.SetActive(false);
+        optionsMenuCanvasGO.SetActive(false);
+
+        InputHandler.UseMenuOpenInput();
+
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+    #endregion
+
+    #region Main Menu Button Actions
+    public void OnOptionsPress()
+    {
+        OpenOptionsMenuHandle();
+    }
+
+    public void OnContinuePress()
+    {
+        Unpause();
+    }
+
+    public void OnQuitPress()
+    {
+        // Placeholder, need to implement
+    }
+    #endregion
+
+    #region Options Menu Button Actions
+    public void OnOptionsBackPress()
+    {
+        OpenMainMenu();
+    }
+    #endregion
+}
