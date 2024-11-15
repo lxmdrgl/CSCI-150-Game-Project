09a3da1061d219c68b028bd7bf7563bf08867586
Alexander Madrigal <lxndrmdrgl@gmail.com>
2024-11-14 16:57:32 -0800

    fixed stun weapon

--- Assets/_Scripts/Weapons/Components/ComponentData/StunDamageData.cs
+++ Assets/_Scripts/Weapons/Components/ComponentData/StunDamageData.cs
@@ -5,15 +5,6 @@ namespace Game.Weapons.Components
 {
     public class StunDamageData : ComponentData<AttackStunDamage>
     {
-        private float amount;
-        private GameObject root;
-
-        public StunDamageData(float amount, GameObject root)
-        {
-            this.amount = amount;
-            this.root = root;
-        }
-
         protected override void SetComponentDependency()
         {
             ComponentDependency = typeof(StunDamage);
