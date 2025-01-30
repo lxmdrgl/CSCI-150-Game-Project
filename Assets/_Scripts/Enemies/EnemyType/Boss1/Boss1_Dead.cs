7868e4b370f7b5c72bd05fc64ffb5f7f875dd62b
LanceH2066 <heinrichlance2066@gmail.com>
2025-01-30 10:20:56 -0800

    Boss1 Scripts (Unfinished)
    created Boss Scripts/data, just waiting for animations

---
+++ Assets/_Scripts/Enemies/EnemyType/Boss1/Boss1_Dead.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1_Dead : DeadState
+{
+    private Boss1 enemy;
+
+    public Boss1_Dead(Entity entity, string animBoolName, D_DeadState stateData, Boss1 enemy) : base(entity, animBoolName, stateData)
+    {
+        this.enemy = enemy;
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
