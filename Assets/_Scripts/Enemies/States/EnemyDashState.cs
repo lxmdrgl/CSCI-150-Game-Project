cf8ac335b0a3d8ad14da06ce475ad1bee084a728
Alexander Madrigal <lxndrmdrgl@gmail.com>
2025-04-29 16:27:46 -0700

    fixed melee dashing enemy

--- Assets/_Scripts/Enemies/States/EnemyDashState.cs
+++ Assets/_Scripts/Enemies/States/EnemyDashState.cs
@@ -29,7 +29,7 @@ public class EnemyDashState : MeleeAttackState
     public EnemyDashState(Entity entity, string animBoolName, GameObject meleeAttackCollider, D_MeleeAttack stateData, Enemy5 enemy) : base(entity, animBoolName, meleeAttackCollider, stateData)
     {
         this.enemy = enemy;
-        dashSpeed = 5f;
+        dashSpeed = 10f;
         jumpSpeed = 20f;
         dashDuration = 0.5f;
     }
