098b8d01e052d42ca7ee3f380b523f96126b0256
Chester Lee <114546460+FinalHope007@users.noreply.github.com>
2025-05-06 11:02:14 -0700

    sprite update for UI

--- Assets/_Scripts/MainMenu/MainMenu.cs
+++ Assets/_Scripts/MainMenu/MainMenu.cs
@@ -13,6 +13,9 @@ public class MainMenu : MonoBehaviour
 {
     [Header("Menu Navigation")]
     [SerializeField] private GameObject mainMenuFirst;
+    [SerializeField] private GameObject playMenuFirst;
+    [SerializeField] private GameObject optionsMenuFirst;
+    [SerializeField] private GameObject accountMenuFirst;
 
     public string GameSceneName;
     public string generatorScene;
